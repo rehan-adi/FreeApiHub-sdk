3552ea4ca37bbd6515f2a7241bdfd368182830c8
Rehan <rehanalire52@gmail.com>
2024-11-07 12:52:33 +0530

    add all endpoint to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
+export { getAllJokes, getJokeById } from "./apis/joke.js";
 export { getAllBooks, getBookById } from "./apis/book.js";
+export { getAllUsers, getUserById } from "./apis/user.js";
+export { getAllStocks, getStockById } from "./apis/stock.js";
 export { getAllQuotes, getQuoteById } from "./apis/quote.js";
+export { getAllProducts, getProductById } from "./apis/product.js";
+export {
+  getAllProgrammingLanguages,
+  getProgrammingLanguageById,
+} from "./apis/programming-language.js";
